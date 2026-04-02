f77f138b31b09ed549ee6b403c1cbaeed3ff96a6
victorkreuz <victor.kreuz@outlook.com>
2026-04-01 22:10:30 -0300

    feat: implementação de url para imagem via cloudinary

--- marmoraria-orcamentos-api/src/main/java/com/marmoraria/orcamentos/service/CloudinaryService.java
+++ marmoraria-orcamentos-api/src/main/java/com/marmoraria/orcamentos/service/CloudinaryService.java
@@ -1,4 +1,20 @@
 package com.marmoraria.orcamentos.service;
 
+import com.cloudinary.Cloudinary;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Map;
+
+@Service
+
 public class CloudinaryService {
+
+    @Autowired
+    Cloudinary cloudinary;
+    public Map<String, Object> salvar(MultipartFile multipartFile) throws IOException {
+        return cloudinary.uploader().upload(multipartFile.getBytes(), Map.of());
+    }
 }
