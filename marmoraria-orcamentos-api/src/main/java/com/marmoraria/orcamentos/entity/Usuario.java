7e8862036b14b4338b37dede8c1668554d31b820
victorkreuz <victor.kreuz@outlook.com>
2026-06-10 16:17:16 -0300

    fix: add @Enumerated(STRING) to TipoUsuario in Usuario entity

--- marmoraria-orcamentos-api/src/main/java/com/marmoraria/orcamentos/entity/Usuario.java
+++ marmoraria-orcamentos-api/src/main/java/com/marmoraria/orcamentos/entity/Usuario.java
@@ -1,9 +1,6 @@
 package com.marmoraria.orcamentos.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -22,5 +19,7 @@ public class Usuario {
 
     private String userName;
     private String senha;
+    
+    @Enumerated(EnumType.STRING)
     private TipoUsuario tipoUsuario;
 }
